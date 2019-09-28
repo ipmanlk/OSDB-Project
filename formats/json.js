408e29e4a53e785b60dd3e34ba908b492a5b700e
ipmanlk <io@navinda.xyz>
2019-09-28 15:49:28 +0530

    change: json output structure

--- formats/json.js
+++ formats/json.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 const build = (data) => {
     try {
         const jsonPath = `${__dirname}/../outputs/json`;
-        fs.writeFileSync(`${jsonPath}/en2sn.json`, JSON.stringify(data.en2sn));
-        fs.writeFileSync(`${jsonPath}/sn2en.json`, JSON.stringify(data.sn2en));
+        const en2sn = {};
+        const sn2en = {};
+        data.en2sn.forEach(row => { en2sn[row.word] = row.definitions.split(",") });
+        data.sn2en.forEach(row => { sn2en[row.word] = row.definitions.split(",") });
+        fs.writeFileSync(`${jsonPath}/en2sn.json`, JSON.stringify(en2sn));
+        fs.writeFileSync(`${jsonPath}/sn2en.json`, JSON.stringify(sn2en));
         console.log(`Success: JSON files have been generated!.`);
     } catch (e) {
         console.log(`Error: Failed to build JSON files!. ${e}`);
